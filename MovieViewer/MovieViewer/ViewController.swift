711501f88293eca9a81c41920e87908f81626257
Tejen Patel <github@tejen.net>
2017-06-04 03:05:17 -0700

    Initial commit - empty project

---
+++ MovieViewer/MovieViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieViewer
+//
+//  Created by Tejen Hasmukh Patel on 6/4/17.
+//  Copyright © 2017 tejen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
